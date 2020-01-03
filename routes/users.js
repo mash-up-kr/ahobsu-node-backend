5ebab2e58dab8ba542bcbedce7bd936d894fba99
Yuni-Q <carnotcycle2@naver.com>
2020-01-04 00:38:43 +0900

    fix checkToken and feat refresh mission

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,8 @@ const response = require('../lib/response');
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
+router.get('/', checkToken, async (req, res, next) => {
+  console.log(111, req.user);
   try {
     const users = await db.users.findAll();
     res.json(response({ data: users }));
@@ -60,7 +61,7 @@ router.put('/:id', checkToken, async (req, res, next) => {
   }
 });
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', checkToken, async (req, res, next) => {
   const id = parseInt(req.params.id, 10);
   if (isNaN(id)) {
     return res.json(response({ status: 412, messgae: 'id가 올바르지 않습니다.' }));
@@ -80,6 +81,47 @@ router.delete('/:id', async (req, res, next) => {
       },
     });
     return res.json(response({ message: '유저를 삭제 했습니다.' }));
+  } catch (e) {
+    console.log(e);
+    res.json(response({ status: 500, messgae: e.message }));
+  }
+});
+
+router.post('/refresh', checkToken, async (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  if (isNaN(id)) {
+    return res.json(response({ status: 412, messgae: 'id가 올바르지 않습니다.' }));
+  }
+  try {
+    const user = await db.users.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      return res.json(response({ status: 404, message: '유저가 존재하지 없습니다.' }));
+    }
+    const date = await moment()
+      .tz('Asia/Seoul')
+      .format('YYYY-MM-DD');
+    if (!user.refreshDate && user.refreshDate < date) {
+      const { id } = req.user;
+      await db.users.update(
+        { refreshDate: date },
+        {
+          where: {
+            id,
+          },
+        },
+      );
+      const sql = 'SELECT * from chocopie.missions ORDER BY RAND() LIMIT 3';
+      const result = await db.sequelize.query(sql, {
+        type: sequelize.QueryTypes.SELECT,
+      });
+      res.json(response({ data: result }));
+    } else {
+      res.json(response({ status: 400, messgae: '갱신 횟수가 모자랄 수 있습니다.' }));
+    }
   } catch (e) {
     console.log(e);
     res.json(response({ status: 500, messgae: e.message }));
