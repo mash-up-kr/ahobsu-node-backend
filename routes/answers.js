6016d3e9820fddcf089ca0197a40d85eae4714f9
YuChocopie <rladbwjd341@naver.com>
2020-01-04 01:51:48 +0900

    feat: update answer
    - 답변 생성및 수정에 이미지 추가 가능하도록 수정하다
      사항에 대한 커밋 메시지를 입력하십시오. '#' 문자로 시작하는

--- routes/answers.js
+++ routes/answers.js
@@ -47,7 +47,6 @@ router.get('/:date', checkToken, async (req, res, next) => {
 
 router.post('/', checkToken, async (req, res, next) => {
   const userId = req.user.id;
-
   AWS.config.update({
     accessKeyId: process.env.AWSAccessKeyId,
     secretAccessKey: process.env.AWSSecretKey,
@@ -62,16 +61,25 @@ router.post('/', checkToken, async (req, res, next) => {
     if (!!checkAnswer) {
       return res.json({ message: '문제에 대한 답변이 존재합니다.' });
     }
-
-    const { file } = files;
+    const image = files.imageUrl;
+    if (!image) {
+      const answer = await db.answers.create({
+        userId: userId,
+        missionId: fields.missionId,
+        content: fields.content,
+        date: fields.date,
+      });
+      res.json({ imageUrl: answer });
+    }
+    const { imageUrl } = files;
     const defaultPath = fileName;
-    const imageUrl = defaultPath + path.parse(file.name).ext;
+    const imageUrl2 = defaultPath + path.parse(imageUrl.name).ext;
     s3.upload(
       {
         Bucket: process.env.buket,
-        Key: imageUrl,
+        Key: imageUrl2,
         ACL: 'public-read',
-        Body: fs.createReadStream(file.path),
+        Body: fs.createReadStream(imageUrl.path),
       },
       (error, result) => {
         if (error) console.log(error);
@@ -79,45 +87,101 @@ router.post('/', checkToken, async (req, res, next) => {
       },
     );
     const baseUrl = 'https://yuchocopie.s3.ap-northeast-2.amazonaws.com/';
-    const imgUrl = baseUrl + imageUrl;
+    const imgUrl = baseUrl + imageUrl2;
 
     const date = moment()
       .tz('Asia/Seoul')
       .format('YYYY-MM-DD');
-    console.log(11, fields.date);
     const answer = await db.answers.create({
       userId: userId,
       missionId: fields.missionId,
-      file: imgUrl,
+      imageUrl: imgUrl,
       content: fields.content,
       date: fields.date,
     });
-
     // unlink tmp files
-    fs.unlinkSync(file.path);
-    res.json({ file: answer });
+    fs.unlinkSync(imageUrl.path);
+    res.json({ imageUrl: answer });
   });
 });
 
 router.put('/:id', checkToken, async (req, res, next) => {
   const id = parseInt(req.params.id, 10);
-  const userId = req.user.id;
-  const { missionId, imageUrl, content } = req.body;
   const answer = await db.answers.findOne({ where: { id } });
-  console.log(111, answer);
-  if (!!answer) {
+  if (!answer) {
+    return res.json({ message: '존재하지않는 answerId.' });
+  }
+  const userId = req.user.id;
+  AWS.config.update({
+    accessKeyId: process.env.AWSAccessKeyId,
+    secretAccessKey: process.env.AWSSecretKey,
+  });
+  const s3 = new AWS.S3();
+  let fileName = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 8; i += 1) fileName += possible.charAt(Math.floor(Math.random() * possible.length));
+  const form = new formidable.IncomingForm();
+  form.parse(req, async (err, fields, files) => {
+    const image = files.imageUrl;
+    if (!image) {
+      console.log(123, '이미지없음');
+      console.log(123, image);
+      await db.answers.update(
+        {
+          userId: userId,
+          missionId: fields.missionId,
+          content: fields.content,
+        },
+        {
+          where: {
+            id,
+          },
+        },
+      );
+      const newAnswer = await db.answers.findOne({ where: { id } });
+      console.log(123, newAnswer);
+      return res.json({ imageUrl: newAnswer });
+    }
+    const { imageUrl } = files;
+    const defaultPath = fileName;
+    const imageUrl2 = defaultPath + path.parse(imageUrl.name).ext;
+    s3.upload(
+      {
+        Bucket: process.env.buket,
+        Key: imageUrl2,
+        ACL: 'public-read',
+        Body: fs.createReadStream(imageUrl.path),
+      },
+      (error, result) => {
+        if (error) console.log(error);
+        else console.log(result);
+      },
+    );
+    const baseUrl = 'https://yuchocopie.s3.ap-northeast-2.amazonaws.com/';
+    const imgUrl = baseUrl + imageUrl2;
+    const date = moment()
+      .tz('Asia/Seoul')
+      .format('YYYY-MM-DD');
+    console.log(11, fields.date);
+
     await db.answers.update(
-      { userId: userId, missionId: missionId, imageUrl: imageUrl, content: content },
+      {
+        userId: userId,
+        missionId: fields.missionId,
+        imageUrl: imgUrl,
+        content: fields.content,
+      },
       {
         where: {
           id,
         },
       },
     );
+    // unlink tmp files
+    fs.unlinkSync(imageUrl.path);
     const newAnswer = await db.answers.findOne({ where: { id } });
-    return res.json({ answer: newAnswer });
-  }
-  res.json({ message: '유효하지 않은 answerId' });
+    return res.json({ imageUrl: newAnswer });
+  });
 });
 
 router.delete('/:id', checkToken, async (req, res, next) => {
