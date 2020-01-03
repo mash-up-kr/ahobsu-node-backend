dc8ec388560703137d53716269934879e2d75d4e
이윤희 <yuniq@woowahan.com>
2020-01-03 11:31:42 +0900

    feat: add setting files

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'extends': 'airbnb-base',
+    rules: {
+      'max-len': ['error', { 'code': 1000 }],
+      'no-param-reassign': 0,
+      'no-console': 0,
+      'global-require': 0
+    }
+  };
+  
