55a91562f8e7753c5dab9774494d551c93552f59
YuChocopie <rladbwjd341@naver.com>
2020-01-03 21:43:59 +0900

    feat: add mission api

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const filesRouter = require('./routes/files');
 const siginInRouter = require('./routes/signIn');
+const missionRouter = require('./routes/missions');
 
 require('dotenv').config();
 const app = express();
@@ -39,6 +40,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/files', filesRouter);
 app.use('/signIn', siginInRouter);
+app.use('/missions', missionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/missions.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const sequelize = require('sequelize');
+
+const db = require('../models');
+const checkToken = require('../lib/checkToken');
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', checkToken, async (req, res, next) => {
+  console.log(req.user.users.email);
+  const sql = 'SELECT * from chocopie.missions ORDER BY RAND() LIMIT 3';
+  const result = await db.sequelize.query(sql, {
+    type: sequelize.QueryTypes.SELECT,
+  });
+  console.log(result);
+});
+
+router.post('/', async (req, res, next) => {
+  const { title, isContent, isImage } = req.body;
+  const missions = await db.missions.create({ title, isContent, isImage });
+  res.json({ missions });
+});
+
+router.put('/:id', async (req, res, next) => {
+  const { title, isContent, isImage } = req.body;
+  const id = parseInt(req.params.id, 10);
+  const mission = await db.missions.findOne({ where: { id } });
+  console.log(111, mission);
+  if (!!mission) {
+    await db.missions.update(
+      { title: title, isContent: isContent, isImage: isImage },
+      {
+        where: {
+          id,
+        },
+      },
+    );
+    const newMission = await db.missions.findOne({ where: { id } });
+    return res.json({ mission: newMission });
+  }
+  res.json({ message: '유효하지 않은 mission id 입니다.' });
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  const mission = await db.missions.findOne({
+    where: {
+      id,
+    },
+  });
+  if (!!mission) {
+    const missions = await db.missions.destroy({
+      where: {
+        id,
+      },
+    });
+    return res.json({ message: '문제를 삭제 했습니다.' });
+  }
+  res.json({ message: '유효하지 않은 mission id 입니다.' });
+});
+
+module.exports = router;
