05fb649b524bf778653fc44ebc7ec7355f5e7d66
이윤희 <yuniq@woowahan.com>
2020-01-03 11:31:42 +0900

    feat: add setting files

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Yuni-Q_TOAST',
+      script: './bin/www',
+      exec_mode: 'cluster',
+      instances: '0',
+      instance_var: 'INSTANCE_ID',
+      env: {
+        NODE_ENV: 'development',
+        NODE_CONFIG_DIR: './config/',
+      },
+      env_staging: {
+        NODE_ENV: 'staging',
+        NODE_CONFIG_DIR: './config/',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        NODE_CONFIG_DIR: './config/',
+      },
+    },
+  ],
+};
