2b63d40f2e72577be84be3819ab1c992da14848a
Yuni-Q <carnotcycle2@naver.com>
2020-01-07 21:01:18 +0900

    feat: add swagger in signin
    Fixes https://github.com/mash-up-kr/Ahobsu-Node-Backend/issues/10

---
+++ swagger/path/signin.js
@@ -0,0 +1,74 @@
+module.exports = {
+  '/signin': {
+    post: {
+      tags: ['signin'],
+      summary: '키 생성',
+      produces: ['application/json'],
+      parameters: [
+        {
+          name: 'body',
+          in: 'body',
+          required: true,
+          schema: {
+            type: 'object',
+            example: {
+              snsId: 1,
+            },
+          },
+        },
+      ],
+      responses: {
+        '200': {
+          schema: {
+            type: 'object',
+            example: {
+              status: 200,
+              message: '',
+              data: {
+                accessToken:
+                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJiaXJ0aGRheSI6IjE5OTctMDEtMTYiLCJlbWFpbCI6Inl1Y2hvY29waWVAZ21haWwuY29tIiwibmFtZSI6IuycoOyglSIsImdlbmRlciI6IuyXrCIsInJlZnJlc2hEYXRlIjpudWxsLCJyZWZyZXNoVG9rZW4iOm51bGwsIm1pc3Npb24iOiJ7XCJkYXRlXCI6XCIyMDIwLTAxLTA3XCIsXCJtaXNzaW9uXCI6W3tcImlkXCI6NCxcInRpdGxlXCI6XCIxMTExXCIsXCJpc0NvbnRlbnRcIjowLFwiaXNJbWFnZVwiOjAsXCJjcmVhdGVkQXRcIjpcIjIwMjAtMDEtMDNUMTc6MzQ6MzUuMDAwWlwiLFwidXBkYXRlZEF0XCI6XCIyMDIwLTAxLTAzVDE3OjM0OjM1LjAwMFpcIn0se1wiaWRcIjo3LFwidGl0bGVcIjpcIjExMTFcIixcImlzQ29udGVudFwiOjAsXCJpc0ltYWdlXCI6MCxcImNyZWF0ZWRBdFwiOlwiMjAyMC0wMS0wN1QxMTozMjo1NC4wMDBaXCIsXCJ1cGRhdGVkQXRcIjpcIjIwMjAtMDEtMDdUMTE6MzI6NTQuMDAwWlwifSx7XCJpZFwiOjMsXCJ0aXRsZVwiOlwiMTExMVwiLFwiaXNDb250ZW50XCI6MCxcImlzSW1hZ2VcIjowLFwiY3JlYXRlZEF0XCI6XCIyMDIwLTAxLTAzVDE3OjM0OjM0LjAwMFpcIixcInVwZGF0ZWRBdFwiOlwiMjAyMC0wMS0wM1QxNzozNDozNC4wMDBaXCJ9XX0iLCJzbnNJZCI6IjEiLCJzbnNUeXBlIjoiZ29vZ2xlIiwiY3JlYXRlZEF0IjoiMjAyMC0wMS0wM1QxNzozNDozNy4wMDBaIiwidXBkYXRlZEF0IjoiMjAyMC0wMS0wN1QxMTozNDoyNC4wMDBaIn0sImlhdCI6MTU3ODM5ODI3MiwiZXhwIjoxNTc5MDAzMDcyfQ.UBYpPIRhUgnwyrYzYeSp-dEJ8KXF63cgJj2Jr4TzOpg',
+                refreshToken:
+                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzbnNJZCI6MSwiaWF0IjoxNTc4Mzk4MjcyLCJleHAiOjE1ODA5OTAyNzJ9.z8V6MFSwDOuKclTv-3hVucFCRtM6fQRtLVBZ-Dn5hlI',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  '/signin/refresh': {
+    post: {
+      tags: ['signin'],
+      summary: '키 재생성',
+      produces: ['application/json'],
+      parameters: [
+        {
+          name: 'Authorization',
+          in: 'header',
+          type: 'string',
+          description: 'API 인증 키',
+          default:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzbnNJZCI6MSwiaWF0IjoxNTc4Mzk4MjcyLCJleHAiOjE1ODA5OTAyNzJ9.z8V6MFSwDOuKclTv-3hVucFCRtM6fQRtLVBZ-Dn5hlI',
+          required: true,
+        },
+      ],
+      responses: {
+        '200': {
+          schema: {
+            type: 'object',
+            example: {
+              status: 200,
+              message: '',
+              data: {
+                accessToken:
+                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJiaXJ0aGRheSI6IjE5OTctMDEtMTYiLCJlbWFpbCI6Inl1Y2hvY29waWVAZ21haWwuY29tIiwibmFtZSI6IuycoOyglSIsImdlbmRlciI6IuyXrCIsInJlZnJlc2hEYXRlIjpudWxsLCJyZWZyZXNoVG9rZW4iOm51bGwsIm1pc3Npb24iOiJ7XCJkYXRlXCI6XCIyMDIwLTAxLTA3XCIsXCJtaXNzaW9uXCI6W3tcImlkXCI6NCxcInRpdGxlXCI6XCIxMTExXCIsXCJpc0NvbnRlbnRcIjowLFwiaXNJbWFnZVwiOjAsXCJjcmVhdGVkQXRcIjpcIjIwMjAtMDEtMDNUMTc6MzQ6MzUuMDAwWlwiLFwidXBkYXRlZEF0XCI6XCIyMDIwLTAxLTAzVDE3OjM0OjM1LjAwMFpcIn0se1wiaWRcIjo3LFwidGl0bGVcIjpcIjExMTFcIixcImlzQ29udGVudFwiOjAsXCJpc0ltYWdlXCI6MCxcImNyZWF0ZWRBdFwiOlwiMjAyMC0wMS0wN1QxMTozMjo1NC4wMDBaXCIsXCJ1cGRhdGVkQXRcIjpcIjIwMjAtMDEtMDdUMTE6MzI6NTQuMDAwWlwifSx7XCJpZFwiOjMsXCJ0aXRsZVwiOlwiMTExMVwiLFwiaXNDb250ZW50XCI6MCxcImlzSW1hZ2VcIjowLFwiY3JlYXRlZEF0XCI6XCIyMDIwLTAxLTAzVDE3OjM0OjM0LjAwMFpcIixcInVwZGF0ZWRBdFwiOlwiMjAyMC0wMS0wM1QxNzozNDozNC4wMDBaXCJ9XX0iLCJzbnNJZCI6IjEiLCJzbnNUeXBlIjoiZ29vZ2xlIiwiY3JlYXRlZEF0IjoiMjAyMC0wMS0wM1QxNzozNDozNy4wMDBaIiwidXBkYXRlZEF0IjoiMjAyMC0wMS0wN1QxMTozNDoyNC4wMDBaIn0sImlhdCI6MTU3ODM5ODI3MiwiZXhwIjoxNTc5MDAzMDcyfQ.UBYpPIRhUgnwyrYzYeSp-dEJ8KXF63cgJj2Jr4TzOpg',
+                refreshToken:
+                  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzbnNJZCI6MSwiaWF0IjoxNTc4Mzk4MjcyLCJleHAiOjE1ODA5OTAyNzJ9.z8V6MFSwDOuKclTv-3hVucFCRtM6fQRtLVBZ-Dn5hlI',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,12 +1,18 @@
 const { paths } = require('./path/base');
-const users = require('./path/users');
 const missions = require('./path/missions');
+const signin = require('./path/signin');
+const users = require('./path/users');
+
+Object.keys(missions).forEach(key => {
+  paths[key] = missions[key];
+});
+
+Object.keys(signin).forEach(key => {
+  paths[key] = signin[key];
+});
 
 Object.keys(users).forEach(key => {
   paths[key] = users[key];
-});
-Object.keys(missions).forEach(key => {
-  paths[key] = missions[key];
 });
 
 module.exports = {
