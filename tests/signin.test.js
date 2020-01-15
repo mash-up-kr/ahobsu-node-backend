79632b04b87b0afc7b550b718e408824a789b944
Yuni-Q <carnotcycle2@naver.com>
2020-01-15 09:49:37 +0900

    feat: add response body

--- tests/signin.test.js
+++ tests/signin.test.js
@@ -2,9 +2,12 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Test /users', () => {
+describe('Test /signin', () => {
   test('should return ok', async () => {
-    const response = await request(app).get('/users');
+    const response = await request(app)
+      .post('/signin')
+      .send({ snsId: 1 });
     expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe(200);
   });
 });
