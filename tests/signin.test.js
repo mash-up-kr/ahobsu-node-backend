41293fde1d3c7c694620c7507fb0a1d59d628c31
Yuni-Q <carnotcycle2@naver.com>
2020-01-15 09:55:34 +0900

    feat: add supertest header

--- tests/signin.test.js
+++ tests/signin.test.js
@@ -11,3 +11,16 @@ describe('Test /signin', () => {
     expect(response.body.status).toBe(200);
   });
 });
+
+describe('Test /signin/refresh', () => {
+  test('should return ok', async () => {
+    const response = await request(app)
+      .post('/signin/refresh')
+      .set(
+        'Authorization',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzbnNJZCI6MSwiaWF0IjoxNTc5MDQ5MjkxLCJleHAiOjE1ODE2NDEyOTF9.pTR8rpi6YA204utBiKIu7Z5jfr6wRyVmfmkrbFyRfE4',
+      );
+    expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe(200);
+  });
+});
