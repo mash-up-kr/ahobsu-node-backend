99bb8e2d80d3cc413d03d9e60651912d5508bc52
Yuni-Q <carnotcycle2@naver.com>
2020-01-15 10:53:59 +0900

    feat: add toBeTruthy

--- __test__/signin.test.js
+++ __test__/signin.test.js
@@ -9,18 +9,26 @@ describe('Test /signin', () => {
       .send({ snsId: 1 });
     expect(response.statusCode).toBe(200);
     expect(response.body.status).toBe(200);
+    expect(response.body.data.accessToken).toBeTruthy();
+    expect(response.body.data.refreshToken).toBeTruthy();
   });
 });
 
 describe('Test /signin/refresh', () => {
   test('should return ok', async () => {
+    const {
+      body: {
+        data: { refreshToken },
+      },
+    } = await request(app)
+      .post('/signin')
+      .send({ snsId: 1 });
     const response = await request(app)
       .post('/signin/refresh')
-      .set(
-        'Authorization',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzbnNJZCI6MSwiaWF0IjoxNTc5MDQ5MjkxLCJleHAiOjE1ODE2NDEyOTF9.pTR8rpi6YA204utBiKIu7Z5jfr6wRyVmfmkrbFyRfE4',
-      );
+      .set('Authorization', refreshToken);
     expect(response.statusCode).toBe(200);
     expect(response.body.status).toBe(200);
+    expect(response.body.data.accessToken).toBeTruthy();
+    expect(response.body.data.refreshToken).toBeTruthy();
   });
 });
