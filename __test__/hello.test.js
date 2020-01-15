4bf3d6a76b0e5ff348590d3d94bf6ba5984c299d
Yuni-Q <carnotcycle2@naver.com>
2020-01-15 10:47:42 +0900

    디렉토리명 및 npm 명령어 변경

--- __test__/hello.test.js
+++ __test__/hello.test.js
@@ -12,3 +12,13 @@ describe('Test /hello', () => {
       });
   });
 });
+
+// request(app)
+//   .post('/')
+//   .field('name', 'my awesome avatar')
+//   .attach('avatar', 'test/fixtures/avatar.jpg')
+//   ...
+
+// expect(users).toHaveLength(3);
+// expect(users).toContainEqual({ id: 1, email: 'user1@test.com' });
+// expect(data.users).not.toContainEqual(user);
