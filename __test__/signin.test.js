86092c135ad85a59f57d779be4006c714cb41693
Yuni-Q <carnotcycle2@naver.com>
2020-01-23 17:30:52 +0900

    signin쪽 조금 수정

--- __test__/signin.test.js
+++ __test__/signin.test.js
@@ -1,4 +1,3 @@
-// tests/status.test.js
 const request = require('supertest');
 const app = require('../app');
 
@@ -6,7 +5,7 @@ describe('Test /api/v1/signin', () => {
   test('should return ok', async () => {
     const response = await request(app)
       .post('/api/v1/signin')
-      .send({ snsId: 1 });
+      .send({ snsId: 1, token: 'aaa' });
     expect(response.statusCode).toBe(200);
     expect(response.body.status).toBe(200);
     expect(response.body.data.accessToken).toBeTruthy();
@@ -30,5 +29,12 @@ describe('Test /api/v1/signin/refresh', () => {
     expect(response.body.status).toBe(200);
     expect(response.body.data.accessToken).toBeTruthy();
     expect(response.body.data.refreshToken).toBeTruthy();
+    expect(hasPostApiV1SigninRefreshKeys);
   });
 });
+
+function hasPostApiV1SigninRefreshKeys(res) {
+  if (!('accessToken' in res.body)) throw new Error('missing accessToken key');
+  if (!('refreshToken' in res.body)) throw new Error('missing refreshToken key');
+  if (!('signUp' in res.body)) throw new Error('missing signUp key');
+}
