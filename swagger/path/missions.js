914ecf2cfb64c1b979cef168847d6ec1f4722ee3
Yuni-Q <carnotcycle2@naver.com>
2020-01-23 19:21:54 +0900

    mission id로 조회 스웨거 작성

--- swagger/path/missions.js
+++ swagger/path/missions.js
@@ -107,7 +107,52 @@ module.exports = {
       },
     },
   },
+
   '/api/v1/missions/{id}': {
+    get: {
+      tags: ['missions'],
+      summary: '미션 수정',
+      produces: ['application/json'],
+      parameters: [
+        {
+          name: 'Authorization',
+          in: 'header',
+          type: 'string',
+          description: 'API 인증 키',
+          default:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJiaXJ0aGRheSI6IjE5OTctMDEtMTYiLCJlbWFpbCI6Inl1Y2hvY29waWVAZ21haWwuY29tIiwibmFtZSI6IuycoOyglSIsImdlbmRlciI6IuyXrCIsInJlZnJlc2hEYXRlIjpudWxsLCJyZWZyZXNoVG9rZW4iOm51bGwsIm1pc3Npb24iOm51bGwsInNuc0lkIjoiMSIsInNuc1R5cGUiOiJnb29nbGUiLCJjcmVhdGVkQXQiOiIyMDIwLTAxLTAzVDE3OjM0OjM3LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIwLTAxLTAzVDE3OjM0OjM3LjAwMFoifSwiaWF0IjoxNTc4MDcyODc5LCJleHAiOjE1Nzg2Nzc2Nzl9.4jBy8Wrj9IukT2H2OU0UdqQjehNXMGio1KAd01z3DvE',
+          required: true,
+        },
+        {
+          name: 'id',
+          in: 'path',
+          type: 'integer',
+          default: 1,
+          description: '미션 ID',
+          required: true,
+        },
+      ],
+      responses: {
+        '200': {
+          schema: {
+            type: 'object',
+            example: {
+              status: 200,
+              message: '',
+              data: {
+                id: 6,
+                title: '문제',
+                isContent: true,
+                isImage: false,
+                cycle: 1,
+                createdAt: '2020-01-03T17:35:02.000Z',
+                updatedAt: '2020-01-03T17:43:10.000Z',
+              },
+            },
+          },
+        },
+      },
+    },
     put: {
       tags: ['missions'],
       summary: '미션 수정',
