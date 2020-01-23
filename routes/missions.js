4c5bf27027d2a0a6894445b3e09b95ee6b4a2197
Yuni-Q <carnotcycle2@naver.com>
2020-01-23 19:18:36 +0900

    mission id로 조회

--- routes/missions.js
+++ routes/missions.js
@@ -190,4 +190,22 @@ router.delete('/:id', checkToken, async (req, res, next) => {
   }
 });
 
+router.get('/:id', checkToken, async (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  if (isNaN(id)) {
+    return res.json(response({ status: 412, message: 'id가 올바르지 않습니다.' }));
+  }
+  try {
+    const mission = await db.missions.findOne({
+      where: {
+        id,
+      },
+    });
+    res.json(response({ data: mission }));
+  } catch (e) {
+    console.log(e);
+    res.json(response({ status: 500, message: e.message }));
+  }
+});
+
 module.exports = router;
