c7c2bed01e8f7d8ae633931537b2eb1982356f18
Yuni-Q <carnotcycle2@naver.com>
2020-01-28 00:30:41 +0900

    fix: fix signin

--- __test__/signin.test.js
+++ __test__/signin.test.js
@@ -8,8 +8,7 @@ describe('Test /api/v1/signin', () => {
       .send({ snsId: '1', snsType: 'apple', token: 'aaa' });
     expect(response.statusCode).toBe(200);
     expect(response.body.status).toBe(200);
-    expect(response.body.data.accessToken).toBeTruthy();
-    expect(response.body.data.refreshToken).toBeTruthy();
+    expect(hasPostApiV1SigninRefreshKeys(response.body.data));
   });
 });
 
@@ -27,8 +26,6 @@ describe('Test /api/v1/signin/refresh', () => {
       .set('Authorization', refreshToken);
     expect(response.statusCode).toBe(200);
     expect(response.body.status).toBe(200);
-    expect(response.body.data.accessToken).toBeTruthy();
-    expect(response.body.data.refreshToken).toBeTruthy();
     expect(hasPostApiV1SigninRefreshKeys(response.body.data));
   });
 });
