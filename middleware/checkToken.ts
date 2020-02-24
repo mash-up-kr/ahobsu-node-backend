057c68f3a4ef1884bf77da5da70f906401249250
Yuni-Q <carnotcycle2@naver.com>
2020-02-24 13:52:38 +0900

    불필요한 콘솔 제거

--- middleware/checkToken.ts
+++ middleware/checkToken.ts
@@ -6,7 +6,6 @@ import { RequestResponseNext } from '../routes';
 
 const checkToken: RequestResponseNext = async (req, res, next) => {
   const token = req.headers.authorization;
-  console.log(9999, token, req.headers);
   if (!token) {
     return res.json(
       response({
