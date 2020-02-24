6cbb8d041c43885029c53aa5b768ec8c8bad08f9
Yuni-Q <carnotcycle2@naver.com>
2020-02-24 17:53:45 +0900

    리팩토링 answers

--- routes/answers/index.ts
+++ routes/answers/index.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import checkToken from '../../middleware/checkToken';
 import imageUploader from '../../middleware/imageUploader';
-import ctrl from './answers.ctroller';
+import answersController from './answers.ctroller';
 
 const router = Router();
 
-router.get('/week', checkToken, ctrl.week);
-router.get('/month', checkToken, ctrl.month);
-router.get('/:date', checkToken, ctrl.date);
-router.post('/', checkToken, imageUploader, ctrl.create);
-router.put('/:id', checkToken, imageUploader, ctrl.update);
-router.delete('/:id', checkToken, ctrl.destroy);
+router.get('/week', checkToken, answersController.week);
+router.get('/month', checkToken, answersController.month);
+router.get('/:date', checkToken, answersController.date);
+router.post('/', checkToken, imageUploader, answersController.create);
+router.put('/:id', checkToken, imageUploader, answersController.update);
+router.delete('/:id', checkToken, answersController.destroy);
 
 export default router;
