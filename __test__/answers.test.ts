6fec5ab338b428ae39e9cf0f8a75d2c60f7b067a
Yuni-Q <carnotcycle2@naver.com>
2020-02-29 15:28:55 +0900

    테스트 케이스 추가

--- __test__/answers.test.ts
+++ __test__/answers.test.ts
@@ -8,6 +8,7 @@ import { postFile } from './files.test';
 import { hasMissionKeys, postMission } from './missions.test';
 import { signin } from './signin.test';
 import { checkStatus, Request } from './util';
+import { getDateString } from '../lib/date';
 
 let token = '';
 const req = request(app);
@@ -26,59 +27,146 @@ beforeAll(async () => {
 describe('answers', () => {
   let response: Response;
   const file = path.join(__dirname, '../public/bigSizeimg.jpeg');
-  const date = moment().format('YYYY-MM-DD');
+  const date = getDateString({});
   let missionId = 0;
-  it('Post /api/v1/answers', async () => {
+
+  it('6개의 파츠 생성', async () => {
+    await Promise.all([
+      // 1
+      postFile({ req, token, file, part: 1 }),
+      // 2
+      postFile({ req, token, file, part: 2 }),
+      // 3
+      postFile({ req, token, file, part: 3 }),
+      // 4
+      postFile({ req, token, file, part: 4 }),
+      // 5
+      postFile({ req, token, file, part: 5 }),
+      // 6
+      postFile({ req, token, file, part: 6 }),
+    ]);
+  });
+  it('Post /api/v1/answers with content', async () => {
+    // 기존에 데이터가 있다면 제거
     response = await getAnswerByDate({ req, date, token });
     checkStatus(response);
-
     if (response.body.data && response.body.data.id) {
       const { id } = response.body.data;
       response = await deleteAnswerById({ req, id, token });
-      checkStatus(response);
+      checkStatus(response, 204);
     }
-    {
-      await Promise.all([
-        // 1
-        postFile({ req, token, file, part: 1 }),
-        // 2
-        postFile({ req, token, file, part: 2 }),
-        // 3
-        postFile({ req, token, file, part: 3 }),
-        // 4
-        postFile({ req, token, file, part: 4 }),
-        // 5
-        postFile({ req, token, file, part: 5 }),
-        // 6
-        postFile({ req, token, file, part: 6 }),
-      ]);
-    }
+
+    // 미션 생성
     const title = '안녕';
     const isContent = true;
     const isImage = false;
     const cycle = 1;
-    const content = 'aaa';
     response = await postMission({ req, token, title, isContent, isImage, cycle });
+    expect(response.body.data.title).toBe(title);
+    expect(response.body.data.isContent).toBe(isContent);
+    expect(response.body.data.isImage).toBe(isImage);
+    expect(response.body.data.cycle).toBe(cycle);
+
+    // content만
     missionId = response.body.data.id;
-    response = await postAnswer({ req, token, missionId, content, file });
-    console.log(3333, response.body);
+    const content = 'aaa';
+    response = await postAnswer({ req, token, missionId, content });
     checkStatus(response, 201);
     expect(hasAnswerKeys(response.body.data));
-    console.log(3333, response.body);
     expect(hasMissionKeys(response.body.data.mission));
     expect(response.body.data.content).toBe(content);
+    expect(response.body.data.imageUrl).toBe(null);
     expect(response.body.data.missionId).toBe(missionId);
+    expect(response.body.data.mission.title).toBe(title);
+    expect(response.body.data.mission.isContent).toBe(isContent);
+    expect(response.body.data.mission.isImage).toBe(isImage);
+    expect(response.body.data.mission.cycle).toBe(cycle);
+  });
+
+  it('Post /api/v1/answers with content and file', async () => {
+    // 기존에 데이터가 있다면 제거
+    response = await getAnswerByDate({ req, date, token });
+    checkStatus(response);
+    if (response.body.data && response.body.data.id) {
+      const { id } = response.body.data;
+      response = await deleteAnswerById({ req, id, token });
+      checkStatus(response, 204);
+    }
+
+    // 미션 생성
+    const title = '안녕';
+    const isContent = false;
+    const isImage = true;
+    const cycle = 1;
+    response = await postMission({ req, token, title, isContent, isImage, cycle });
+    checkStatus(response, 201);
+    expect(response.body.data.title).toBe(title);
+    expect(response.body.data.isContent).toBe(isContent);
+    expect(response.body.data.isImage).toBe(isImage);
+    expect(response.body.data.cycle).toBe(cycle);
+
+    // content, file
+    missionId = response.body.data.id;
+    const content = 'aaa';
+    response = await postAnswer({ req, token, missionId, content, file });
+    checkStatus(response, 201);
+    expect(hasAnswerKeys(response.body.data));
+    expect(hasMissionKeys(response.body.data.mission));
+    expect(response.body.data.content).toBe(content);
+    expect(response.body.data.imageUrl.length).toBeTruthy();
+    expect(response.body.data.missionId).toBe(missionId);
+    expect(response.body.data.mission.title).toBe(title);
+    expect(response.body.data.mission.isContent).toBe(isContent);
+    expect(response.body.data.mission.isImage).toBe(isImage);
+    expect(response.body.data.mission.cycle).toBe(cycle);
+  });
+
+  it('Post /api/v1/answers with file', async () => {
+    // 기존에 데이터가 있다면 제거
+    response = await getAnswerByDate({ req, date, token });
+    checkStatus(response);
+    if (response.body.data && response.body.data.id) {
+      const { id } = response.body.data;
+      response = await deleteAnswerById({ req, id, token });
+      checkStatus(response, 204);
+    }
+
+    // 미션 생성
+    const title = '안녕';
+    const isContent = false;
+    const isImage = true;
+    const cycle = 1;
+    response = await postMission({ req, token, title, isContent, isImage, cycle });
+    checkStatus(response, 201);
+    expect(response.body.data.title).toBe(title);
+    expect(response.body.data.isContent).toBe(isContent);
+    expect(response.body.data.isImage).toBe(isImage);
+    expect(response.body.data.cycle).toBe(cycle);
+
+    // file만
+    missionId = response.body.data.id;
+    response = await postAnswer({ req, token, missionId, file });
+    checkStatus(response, 201);
+    expect(hasAnswerKeys(response.body.data));
+    expect(hasMissionKeys(response.body.data.mission));
+    expect(response.body.data.content).toBe(null);
+    expect(response.body.data.imageUrl).toBeTruthy();
+    expect(response.body.data.missionId).toBe(missionId);
+    expect(response.body.data.mission.title).toBe(title);
+    expect(response.body.data.mission.isContent).toBe(isContent);
+    expect(response.body.data.mission.isImage).toBe(isImage);
+    expect(response.body.data.mission.cycle).toBe(cycle);
   });
 
   it('Post /api/v1/answers Exist', async () => {
     const content = 'aaa';
-    const existResponse = await postAnswer({ req, token, missionId, content, file });
+    const existResponse = await postAnswer({ req, token, missionId, content });
     checkStatus(existResponse, 400);
     expect(existResponse.body.message).toBe('해당날짜에 답변이 존재합니다.');
   });
 
   it('Get /api/v1/answers/{date}', async () => {
-    const date = moment().format('YYYY-MM-DD');
+    const date = getDateString({});
     response = await getAnswerByDate({ req, date, token });
     checkStatus(response);
     expect(hasAnswerKeys(response.body.data));
@@ -120,23 +208,6 @@ describe('answers', () => {
     // expect(haveDateResponse.body.data.answers.length > 0).toBeTruthy();
     expect(haveDateResponse.body.data.date).toBe(`${month.format('YYYY-MM')}-01`);
   });
-
-  it('Get /api/v1/answers/{date}', async () => {
-    const date = moment().format('YYYY-MM-DD');
-    response = await getAnswerByDate({ req, date, token });
-    console.log(22222, response.body);
-    checkStatus(response);
-    expect(hasAnswerKeys(response.body.data));
-    expect(hasMissionKeys(response.body.data.mission));
-  });
-
-  it('Delete /api/v1/answers/{id}', async () => {
-    console.log(11111, response.body);
-    const { id } = response.body.data;
-    response = await deleteAnswerById({ req, id, token });
-    checkStatus(response, 204);
-    expect(response.body.message).toBe('답변을 삭제 했습니다.');
-  });
 });
 
 const hasAnswerKeys = (data: Answers) => {
@@ -167,8 +238,8 @@ const postAnswer = async ({
   req: Request;
   token: string;
   missionId: number;
-  content: string | null;
-  file: string | null;
+  content?: string | null;
+  file?: string | null;
 }) => {
   // 둘다 있는 경우
   if (!!content && file) {
@@ -206,14 +277,29 @@ const putAnswer = async ({
   req: Request;
   token: string;
   id: number;
-  content: string;
-  file: string;
+  content?: string;
+  file?: string;
 }) => {
-  return req
-    .put(`/api/v1/answers/${id}`)
-    .set('Authorization', token)
-    .field('content', content)
-    .attach('file', file);
+  if (!!content && !!file) {
+    return req
+      .put(`/api/v1/answers/${id}`)
+      .set('Authorization', token)
+      .field('content', content)
+      .attach('file', file);
+    // content만 있는 경우
+  } else if (!file && !!content) {
+    return req
+      .put(`/api/v1/answers/${id}`)
+      .set('Authorization', token)
+      .field('content', content);
+    // file만 있는 경우
+  } else if (!content && !!file) {
+    return req
+      .put(`/api/v1/answers/${id}`)
+      .set('Authorization', token)
+      .attach('file', file);
+  }
+  throw 'content, file 둘 중 하나는 있어야 합니다.';
 };
 
 const getAnswersWeek = async ({ req, token }: { req: Request; token: string }) => {
