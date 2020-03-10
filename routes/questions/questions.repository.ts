0c4ebd948ebb3ca662159b4330b1cf66cce7fe31
YuChocopie <rladbwjd341@naver.com>
2020-03-10 23:19:27 +0900

    refactor

--- routes/questions/questions.repository.ts
+++ routes/questions/questions.repository.ts
@@ -9,15 +9,20 @@ export const getQuestions = async ({ number }: { number: string }) => {
   const pageNum = parseInt(number, 10); // 요청 페이지 넘버
   let offset = 0;
   const limit = 5;
+  const sum = (await db.questions.findAll()).length;
+  const questions = await db.questions.findAll({
+    offset,
+    limit,
+  });
+  console.log(333);
+  console.log(sum);
 
   if (pageNum > 1) {
     offset = limit * (pageNum - 1);
   }
+
   return {
-    sum: db.questions.findAll.createQuestion.length,
-    question: db.questions.findAll({
-      offset,
-      limit,
-    }),
+    sum,
+    questions,
   };
 };
