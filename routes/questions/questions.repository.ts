a628b67a8314cda73036fc3b23081b13c33973d2
YuChocopie <rladbwjd341@naver.com>
2020-03-11 00:22:01 +0900

    sum 함수분리, 변수초기값 설정 변경

--- routes/questions/questions.repository.ts
+++ routes/questions/questions.repository.ts
@@ -5,20 +5,25 @@ export const createQuestion = async ({ content }: { content: string }) => {
     content,
   });
 };
-export const getQuestions = async ({ page, listCount }: { page: string; listCount: string }) => {
+export const getQuestionSum = async () => {
+  const sum = (await db.questions.findAll()).length;
+  return {
+    sum,
+  };
+};
+
+export const getQuestions = async ({ page = '1', listCount = '20' }: { page: string; listCount: string }) => {
   const pageNum = parseInt(page, 10); // 요청 페이지 넘버
   const limit = parseInt(listCount, 10); // 요청 data 갯수
   let offset = 0;
   if (pageNum > 1) {
     offset = limit * (pageNum - 1);
   }
-  const sum = (await db.questions.findAll()).length;
   const questions = await db.questions.findAll({
     offset,
     limit,
   });
   return {
-    sum,
     questions,
   };
 };
