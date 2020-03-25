ac6652e759493bd92628767990e6d354925a9331
Yuni-Q <carnotcycle2@naver.com>
2020-03-25 14:23:00 +0900

    fix: fix database

--- config/config.ts
+++ config/config.ts
@@ -19,7 +19,7 @@ const config: IConfigGroup = {
   development: {
     username: process.env.DB_USERNAME!,
     password: process.env.DB_PASSWORD!,
-    database: 'test',
+    database: 'chocopie',
     host: process.env.DB_HOST!,
     dialect: 'mysql',
     timezone: '+09:00',
@@ -41,7 +41,7 @@ const config: IConfigGroup = {
   test: {
     username: 'root',
     password: 'root',
-    database: 'test',
+    database: 'chocopie',
     storage: ':memory:',
     host: 'localhost',
     dialect: 'sqlite',
