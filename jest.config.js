b94662ffe9d4d17ff8a65b4514804ad5a979f384
Yuni-Q <carnotcycle2@naver.com>
2020-06-12 15:41:47 +0900

    오타 수정

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   globals: {
     'ts-jest': {
-      compiler: 'ttypescript',
+      compiler: 'typescript',
       tsConfig: {
         importHelpers: true,
       },
